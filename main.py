d192b53a7dd4d33ea5b3ace3d000cbdb03934cd6
Javimo <javy.mon.or@gmail.com>
2023-09-05 12:46:19 +0200

    Main Window added

---
+++ main.py
@@ -0,0 +1,19 @@
+import sys
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("PyTexLab")
+
+        #self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
